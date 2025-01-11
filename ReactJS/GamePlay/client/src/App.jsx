d0f2264a8d9f25579c6a0349dbe63a0d4103027a
pchinovsky <144697881+pchinovsky@users.noreply.github.com>
2025-01-11 14:39:46 +0200

    Main routing

--- ReactJS/GamePlay/client/src/App.jsx
+++ ReactJS/GamePlay/client/src/App.jsx
@@ -3,6 +3,10 @@ import Header from "./components/header/header";
 import Home from "./components/home/home";
 import Login from "./components/login/login";
 import Register from "./components/register/Register";
+import Catalogue from "./components/catalogue/Catalogue";
+import Create from "./components/create/Create";
+import { Routes, Route } from "react-router-dom";
+import Details from "./components/details/Details";
 
 function App() {
     return (
@@ -11,9 +15,32 @@ function App() {
                 <Header></Header>
 
                 <main id="main-content">
-                    <Home></Home>
-                    <Login></Login>
-                    <Register></Register>
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={<Home />}
+                        />
+                        <Route
+                            path="/catalogue"
+                            element={<Catalogue />}
+                        />
+                        <Route
+                            path="/catalogue/create"
+                            element={<Create />}
+                        />
+                        <Route
+                            path="/login"
+                            element={<Login />}
+                        />
+                        <Route
+                            path="/register"
+                            element={<Register />}
+                        />
+                        <Route
+                            path="/details/:gameDetails"
+                            element={<Details />}
+                        />
+                    </Routes>
                 </main>
             </div>
         </>
